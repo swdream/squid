42ff71f801ef5d54cd6f1e73324e81defc2090a5
swdream <ngtthanh1010@gmail.com>
2017-07-19 12:38:04 +0700

    make sure all neccessary dirs already exist before starting squid

--- Dockerfile
+++ Dockerfile
@@ -4,16 +4,16 @@ MAINTAINER Swdream "ngtthanh1010@gmail.com"
 ENV SQUID_CONFIG_DIR=/etc/squid
 
 RUN apk update && \
-    apk add --no-cache squid=3.5.23-r0 apache2-utils
+    apk add --no-cache su-exec squid=3.5.23-r0 apache2-utils
 
 RUN mv ${SQUID_CONFIG_DIR}/squid.conf ${SQUID_CONFIG_DIR}/squid.conf.dist
-RUN  mkdir -p /var/cache/squid
-RUN chown -R squid:squid /var/cache/squid
 
 COPY entrypoint.sh /
 COPY conf/squid.conf /etc/squid/squid.conf
 
 EXPOSE 3128
+
+VOLUME ["/var/cache/squid"]
 
 ENTRYPOINT ["/entrypoint.sh"]
 CMD ["squid"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,43 @@ set -e
 SQUID_USERNAME=${USERNAME:-squip}
 SQUID_PASSWORD=${PASSWORD:-squip}
 
+QUID_CONFIG_DIR=/etc/squid
+SQUID_LOG_DIR=/var/log/squid
+SQUID_CACHE_DIR=/var/cache/squid
+
 # default behaviour is to launch squid
 htpasswd -bc /etc/squid/passwd "${SQUID_USERNAME}" "${SQUID_PASSWORD}"
+chmod 644 /etc/squid/passwd
+
+create_log_dir() {
+  if [[ ! -d ${SQUID_LOG_DIR} ]]; then
+    echo "creating log dir"
+    mkdir -p ${SQUID_LOG_DIR}
+  fi
+  chmod -R 755 ${SQUID_LOG_DIR}
+  chown -R squid:squid ${SQUID_LOG_DIR}
+}
+
+create_cache_dir() {
+  if [[ ! -d ${SQUID_CACHE_DIR} ]]; then
+    echo "creating cache dir"
+    mkdir -p ${SQUID_CACHE_DIR}
+  fi
+  chmod -R 755 ${SQUID_CACHE_DIR}
+  chown -R squid:squid ${SQUID_CACHE_DIR}
+}
+
+create_log_dir
+create_cache_dir
 
 SQUID_VERSION=$(/usr/sbin/squid -v | grep Version | awk '{ print $4 }')
 if [ "$1" == "squid" ]; then
   echo "Staring squid [${SQUID_VERSION}]"
-  /usr/sbin/squid -z
-  /usr/sbin/squid
+  if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
+    echo "Initializing cache..."
+    exec /sbin/su-exec root /usr/sbin/squid -z
+  fi
+  exec /sbin/su-exec root /usr/sbin/squid -N
 else
   exec "$@"
 fi
